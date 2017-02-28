4107bbc12f48a8fe3fb67844d990bb74be5201a9
rsstdd <rosstod@gmail.com>
2017-02-27 21:38:38 -0800

    Hello, world in C++

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bulls_cows
+//
+//  Created by Ross Todd on 2/27/17.
+//  Copyright © 2017 Ross Todd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
